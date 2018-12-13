642355afcdf38bc6be7c2110fc9be73f231fe657
Ozzy <cfhamlet@gmail.com>
2018-12-13 18:15:15 +0800

    test: base client network

--- tests/test_tornado/test_tornado_client.py
+++ tests/test_tornado/test_tornado_client.py
@@ -1,6 +1,34 @@
-from os_dbnetget.clients.tornado_client import TornadoClient
+import socket
+
+from tornado.testing import AsyncTestCase, gen_test
+
+import pytest
+from os_dbnetget.clients.tornado_client import TornadoClient, TornadoClientPool
+from os_dbnetget.exceptions import RetryLimitExceeded, ResourceLimit
 
 
-def test_tornado_client_wrong_endpoint():
-    c = TornadoClient('unknowhost.com', 8012)
-    # TODO
+class TestTornadoClient(AsyncTestCase):
+    @gen_test
+    def test_client_with_wrong_endpoint(self):
+        host = 'notexisthostname.com'
+        port = 8012
+        c = TornadoClient(host, port)
+        with pytest.raises(socket.gaierror):
+            yield c.execute(None)
+
+    @gen_test
+    def test_client_connect_fail(self):
+        host = 'localhost'
+        port = 8012
+        c = TornadoClient(host, port, retry_max=1, retry_interval=0)
+
+        with pytest.raises(RetryLimitExceeded):
+            yield c.execute(None)
+
+    @gen_test
+    def test_client_pool_try_out(self):
+
+        endpoints = ['localhost:8011', 'localhost:8012']
+        pool = TornadoClientPool(endpoints, retry_max=1, retry_interval=0)
+        with pytest.raises(ResourceLimit):
+            yield pool.execute(None)
