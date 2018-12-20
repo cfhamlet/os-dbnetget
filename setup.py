3a275665c7301a4ba5fc54549ac0a139bdaef614
Ozzy <cfhamlet@gmail.com>
2018-12-20 17:33:54 +0800

    add: modify LICENSE, add python_requires in setup.py

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(
     author_email='cfhamlet@gmail.com',
     url='https://github.com/cfhamlet/os-dbnetget',
     install_requires=open('requirements.txt').read().split('\n'),
+    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,!=3.5.*',
     zip_safe=False,
     entry_points={
         'console_scripts': ['os-dbnetget = os_dbnetget.cmdline:execute']
